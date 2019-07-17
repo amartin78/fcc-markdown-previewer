1b35d3958232cf80a0040e5cb32b18216c3b9944
antonio <ammj24@hotmail.com>
2019-07-17 18:10:54 +0100

    marked library and preview

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import marked from 'marked';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Main extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '# Header 1\n## Header 2\n[link](https://google.es) \n\n' +
+            'some inline text\n\n' + 
+            ' ``` inline code```\n\n' + 
+            '    //comment\n    <div>\n      <p>some text</p>\n    </div>\n' + 
+            '* li 1 \n* li 2\n* li 3\n' +
+            '># This is a blockquote \n>blockquote 1 \n\n >blockquote 2\n\n' + 
+            '![background image](http://subtlepatterns2015.subtlepatterns.netdna-cdn.com/patterns/squares.png "title")\n\n' + 
+            '**this is bold text**'
+        }
+        this.handleChange = this.handleChange.bind(this);
+        // this.initialText = this.initialText.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            input: event.target.value,
+        });
+    }
+
+    // initialText = () => {
+    //     this.setState({
+    //         input: '',
+    //     });
+    // }
+
+    componentDidMount() {
+        // this.initialText();
+    }
+
+    render() {
+        // const arr = this.state.input.split('\n').map(s => s + "\n");;
+        // console.log(arr[0])
+
+        return (
+            <div>
+                <textarea id="editor" rows="10" cols="60" value={this.state.input} onChange={this.handleChange}></textarea>
+                <div id="preview" dangerouslySetInnerHTML={{__html:marked(this.state.input)}}></div>
+            </div>
+        );
+    }
+}
+
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
+
+
