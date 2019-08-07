334f441fa31c15468cf1216469226b1f07f322d7
antonio <ammj24@hotmail.com>
2019-08-07 20:50:11 +0100

    image and code css

--- src/index.js
+++ src/index.js
@@ -21,11 +21,11 @@ class Main extends React.Component {
         this.state = {
             input: '# Header 1\n## Header 2\n[link](https://google.es) \n\n' +
             'some inline text\n\n' + 
-            ' ``` inline code```\n\n' + 
+            ' ```inline code```\n\n' + 
             '    //comment\n    <div>\n      <p>some text</p>\n    </div>\n' + 
             '* li 1 \n* li 2\n* li 3\n' +
             '># This is a blockquote \n>blockquote 1 \n\n >blockquote 2\n\n' + 
-            '![background image](http://subtlepatterns2015.subtlepatterns.netdna-cdn.com/patterns/squares.png "title")\n\n' + 
+            '![background image](https://upload.wikimedia.org/wikipedia/commons/4/46/%27Anas_hybrid%27_Mallard_at_Henham_Essex_England_03.jpg "title")\n\n' + 
             '**this is bold text**'
         }
         this.handleChange = this.handleChange.bind(this);
